ef1ced877521af296c1bec4238db9cffc0676f1f
jeff-eng <me@jeffeng.com>
2024-11-09 15:14:27 -0800

    Most of unit tests from App were moved to ChoreFormControls. All tests pass.

---
+++ src/components/ChoreFormControls.test.jsx
@@ -0,0 +1,63 @@
+import { describe, it, expect, beforeEach } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+
+describe('ChoreForm component', () => {
+  beforeEach(() => {
+    render(<App />);
+  });
+
+  it('should render an input field (with placeholder text) to add a new chore', () => {
+    const input = screen.getByPlaceholderText(/Do dishes/i);
+
+    expect(input).toBeInTheDocument();
+  });
+
+  // Test that a button to add a chore to the list is in the document
+  it('should render a button that has "->" as text', () => {
+    // Check for button in the document
+    const addButton = screen.getByRole('button', { name: '->' });
+
+    // Assertion
+    expect(addButton).toBeInTheDocument();
+  });
+
+  // Test that a button to clear all chores is in the document
+  it('should render a button with text "X" that clears all chores', () => {
+    // Check for button in the document
+    const clearAllButton = screen.getByRole('button', { name: 'X' });
+
+    expect(clearAllButton).toBeInTheDocument();
+  });
+
+  // Test adding a new chore to the list
+  it('should add a new chore to the list and clear input after adding chore', async () => {
+    // Get references to the input and submit button
+    const input = screen.getByPlaceholderText(/Do dishes/i);
+    const button = screen.getByRole('button', { name: '->' });
+
+    // Simulate input entry and button click
+    await userEvent.type(input, 'Clean kitchen');
+    await userEvent.click(button);
+
+    // Assert that the the input 'Clean kitchen' is added
+    const listItem = screen.getByText(/clean kitchen/i);
+    expect(listItem).toBeInTheDocument();
+
+    expect(input).toHaveValue('');
+  });
+
+  // Clear all button
+  it('no chores should be displayed after clicking clear all button', async () => {
+    const clearAllButton = screen.getByRole('button', { name: 'X' });
+    const unorderedList = screen.getByRole('list');
+
+    // Simulate user behavior
+    await userEvent.click(clearAllButton);
+
+    // Assertion
+    const listItems = unorderedList.querySelectorAll('li');
+    expect(listItems.length).toBe(0);
+  });
+});
