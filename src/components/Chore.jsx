442b4b7882d1674789ee92ef04df568f2b9079e5
jeff-eng <me@jeffeng.com>
2024-11-13 17:04:53 -0800

    Added class names to list item elements for CSS styling.

--- src/components/Chore.jsx
+++ src/components/Chore.jsx
@@ -1,9 +1,11 @@
+import '../styles/chore.css';
+
 export default function Chore({ description, handleDeleteClicked }) {
   return (
-    <li className="chores-list__item">
+    <li className="chores-list__item box-shadow">
       {description}
       <button
-        className=""
+        className="chores-list__delete-button box-shadow"
         type="button"
         onClick={() => handleDeleteClicked(description)}
       >
